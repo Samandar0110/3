a875c14ff70b9c5fae914c2e072c34fb9104fec3
Samandar0110 <mahmoda21031964@gmail.com>
2026-02-09 17:55:39 +0500

    add

---
+++ index.js
@@ -0,0 +1,29 @@
+// const elHeader = document.getElementById('header');
+// console.log(elHeader);
+
+// const elHeader1 = document.getElementById('header');
+// console.log(elHeader1.innerHTML);
+
+// const elHeader2 = document.getElementById('header');
+// elHeader2.innerHTML = 'New Site Header';
+
+// const elHeader1 = document.getElementById('header');
+// console.log(elHeader1.innerHTML);
+
+// const elText = document.getElementsByTagName('p');
+// console.log(elHeader.textContent);
+
+// let elHeader = document.querySelector('h1');
+// console.log(elHeader.textContent);
+// console.log(elHeader.innerText);    
+
+// const elText = document.getElementsByClassName('text');
+// console.log(elText)
+
+let elHeaderByTag = document.querySelector('p');
+let elHeaderById = document.querySelector('header');
+let elHeaderByClassName = document.querySelector('headers2');
+
+console.log(elHeaderByTag);
+console.log(elHeaderById);
+console.log(elHeaderByClassName);
